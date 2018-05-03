8642c80c23eaac9d42ce03e5b7bca1abbfc824b5
rpGIS <rpgiesler@gmail.com>
2018-05-03 09:19:00 -0400

    we back

--- httpServer.js
+++ httpServer.js
@@ -32,64 +32,61 @@ var app = express();
 		res.send("hello world from the HTTP server");
 	});
 
-	var fs = require('fs');
+// read in the file and force it to be a string by adding “” at the beginning
+var fs = require('fs')
+var configtext =""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
 
-	// read in the file and force it to be a string by adding “” at the beginning
-	var configtext =""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
-	// now convert the configruation file into the correct format -i.e. a name/value
-	//pair array
-	var configarray = configtext.split(",");
-	var config = {};
-	for (var i = 0; i < configarray.length; i++) {
-	 var split = configarray[i].split(':');
-	 config[split[0].trim()] = split[1].trim();
+for (var i = 0; i < configarray.length; i++) {
+	var split = configarray[i].split(':');
+	config[split[0].trim()] = split[1].trim();
+}
+
+var pg = require('pg');
+var pool = new pg.Pool(config);
+
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+	if(err){
+		console.log("not able to get connection "+ err);
+		res.status(400).send(err);
 	}
-	var pg = require('pg');
-	var pool = new pg.Pool(config);
-
-
-	app.get('/postgistest', function (req,res) {
-	pool.connect(function(err,client,done) {
-	 if(err){
-	 console.log("not able to get connection "+ err);
-	 res.status(400).send(err);
-	 }
-	 client.query('SELECT name FROM united_kingdom_counties'
-	,function(err,result) {
-	 done();
-	 if(err){
-	 console.log(err);
-	 res.status(400).send(err);
-	 }
-	 res.status(200).send(result.rows);
-	 });
-	 });
+	client.query('SELECT name FROM united_kingdom_counties'
+		,function(err,result) {
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			res.status(200).send(result.rows);
+		});
 	});
-	app.get('/getPOI', function (req,res) {
-	 pool.connect(function(err,client,done) {
-	 if(err){
-	 console.log("not able to get connection "+ err);
-	 res.status(400).send(err);
-	 }
-	 // use the inbuilt geoJSON functionality
-	 // and create the required geoJSON format using a query adapted from here:http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-FeatureCollections-with-JSON-and-PostGIS-functions.html,accessed 4th January 2018
-	 // note that query needs to be a single string with no line breaks so builtit up bit by bit
-	 var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
-	 querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg.geom)::json As geometry, ";
-	 querystring = querystring + "row_to_json((SELECT l FROM (SELECT id, name, category) As l )) As properties";
-	 querystring = querystring + " FROM united_kingdom_poi As lg limit 100 ) As f ";
-	 console.log(querystring);
-	 client.query(querystring,function(err,result){
-	 //call `done()` to release the client back to the pool
-	 done();
-	 if(err){
-	 console.log(err);
-	 res.status(400).send(err);
-	 }
-	 res.status(200).send(result.rows);
-	 });
-	 });
-	});
+});
+app.get('/getPOI', function (req,res) {
+ pool.connect(function(err,client,done) {
+ if(err){
+ console.log("not able to get connection "+ err);
+ res.status(400).send(err);
+ }
+
+var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM ";
+querystring = querystring + "(SELECT 'Feature' As type , ST_AsGeoJSON(lg.geom)::json As geometry, ";
+querystring = querystring + "row_to_json((SELECT l FROM (SELECT id, name, category) As l )) As properties";
+querystring = querystring + " FROM united_kingdom_poi As lg limit 100 ) As f ";
+ console.log(querystring);
+ client.query(querystring,function(err,result){
+ //call `done()` to release the client back to the pool
+ done();
+ if(err){
+ console.log(err);
+ res.status(400).send(err);
+ }
+ res.status(200).send(result.rows);
+ });
+ });
+});
 	// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
   app.get('/:name1', function (req, res) {
   // run some server-side code
@@ -99,7 +96,6 @@ var app = express();
   // the res is the response that the server sends back to the browser - you will see this text in your browser window
   res.sendFile(__dirname + '/'+req.params.name1);
 });
-
 
   // the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
   app.get('/:name1/:name2', function (req, res) {
